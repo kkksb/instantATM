83f847313dd9397b216d23d013f58ee98234bc7d
kkksb <kkex5027@gmail.com>
2021-12-02 21:27:44 +0900

    add: ざっくりと作ってみる

---
+++ docs/research.md
@@ -0,0 +1,19 @@
+# 参考にしたもの
+
+[ScriptsLab \- C言語による銀行シミュレーション](http://scrlab.webcrow.jp/modules/developlog/details.php?bid=14)
+
+# C言語の実行
+
+```
+$ gcc hello.c -o hello
+```
+[プログラミング\(1\)「コンパイルと実行」](https://polymer.apphy.u-fukui.ac.jp/~koishi/lecture/program1.php?type=nyumon)
+
+## 文字コードの指定
+
+コンソールで文字化けしたら、以下を実行。
+```
+$ chcp 65001
+```
+
+[Windows10のgccでUTF8で作ったコードが文字化けする場合の対処 \| 電脳産物](https://dianxnao.com/windows10%E3%81%AEgcc%E3%81%A7utf8%E3%81%A7%E4%BD%9C%E3%81%A3%E3%81%9F%E3%82%B3%E3%83%BC%E3%83%89%E3%81%8C%E6%96%87%E5%AD%97%E5%8C%96%E3%81%91%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%AE%E5%AF%BE/)
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int balance = 10000; // balanceは残高という意味
+    int choicedMenu;
+
+    printf("〇〇銀行ATMへようこそ！\n");
+    printf("お取引内容を以下から選択してください。(1~4で回答)\n");
+    printf("1: 残高照会 2: 入金 3: 引き出し 4: 終了\n");
+    scanf("%d", &choicedMenu);
+
+    switch (choicedMenu)
+    {
+    case 1:
+        printf("残高の照会をします\n");
+        break;
+    case 2:
+        printf("入金の手続きをします\n");
+        break;
+    case 3:
+        printf("引き出し手続きをします\n");
+        break;
+    case 4:
+        printf("残高の照会をします\n");
+        break;
+
+    default:
+        printf("0~4にて入力してください。メニューに戻ります。");
+        break;
+    }
+
+    return 0;
+}
